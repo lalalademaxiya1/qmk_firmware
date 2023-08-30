b86aa07f7c40685fc176d73b993e71f010befdce
lalalademaxiya1 <2831039915@qq.com>
2023-08-30 10:12:04 +0800

    Update k17 pro

--- keyboards/keychron/k17_pro/ansi_encoder/white/keymaps/default/keymap.c
+++ keyboards/keychron/k17_pro/ansi_encoder/white/keymaps/default/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [MAC_FN]   = {ENCODER_CCW_CW(BL_DOWN, BL_UP)},
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [WIN_FN]   = {ENCODER_CCW_CW(BL_DOWN, BL_UP)},
 };
 #endif // ENCODER_MAP_ENABLE
